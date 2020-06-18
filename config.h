9e81008f0806c0c032b02cd536c9803c6139c484
Kumar Tatsat <rvk8878@gmail.com>
2020-06-18 23:43:35 +0530

    added 'hide vacant tags' patch | looks clean now :)

--- config.h
+++ config.h
@@ -19,6 +19,7 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
+	"zsh", "-c", "/home/pr0ph4t/.scripts/status.sh", NULL,
 	"zsh", "-c", "xset r rate 450 50", NULL,
 	"zsh", "-c", "compton -b", NULL,
 	"zsh", "-c", "nitrogen --restore", NULL,
@@ -75,7 +76,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -86,7 +87,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
